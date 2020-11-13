e1325a4052cf6a4144f767a98b5764ae297bb4ca
NayeraMohamed <nayeramoh7@gmail.com>
2020-11-13 13:54:39 +0200

    added function calls in main

--- Task 2/main.m
+++ Task 2/main.m
@@ -1,1 +1,11 @@
-%initial commit
+%I = imread('I.bmp');
+%M = MeanMask(3, 5);
+%M = Gauss1(Size,Sig);
+%M = Gauss2(Sig);
+%M = LaplacianSharp();
+%M = Sobel('H');
+%M = Sobel('V');
+%G = EdgeMagnit(I);
+
+%Out = LinearFilter(I, M, Postproc);
+%figure, imshow(Out), title('Filter Applied');
