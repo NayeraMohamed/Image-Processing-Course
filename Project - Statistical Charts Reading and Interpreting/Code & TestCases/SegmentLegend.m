af5cb4c0056d0d91e15354a7e30616b82a97bb41
Reem Osama <32741801+ReemOsama99@users.noreply.github.com>
2021-03-26 11:06:13 +0200

    Update SegmentLegend.m
    CodeAdded

--- Project - Statistical Charts Reading and Interpreting/Code & TestCases/SegmentLegend.m
+++ Project - Statistical Charts Reading and Interpreting/Code & TestCases/SegmentLegend.m
@@ -1,4 +1,26 @@
+function [Icropped] = SegmentLegend(I)
+R = I;
+BW = im2bw(R,0.95);
 
+%Reverse, erode then dilate to segment the bars and circle alone
+BW=~(BW);
+se=strel('square',30);
+  d = imerode(BW,se);
+  d = imdilate(d,se);
+  
+%Re-reverse (to have a black shape) => then loop and whiten this black shape from Original img  
+d=~(d);
+  for i=1:size(d,1)
+      for j=1:size(d,2)
+          if d(i,j)==0 || i==1 || j==1 || i==size(d,1) || j==size(d,2)
+              R(i,j,1)=255;
+              R(i,j,2)=255;
+              R(i,j,3)=255;
+          end
+      end
+  end
+
+d=im2bw(R,0.7);
 % Detect black line
 BW = d < 1;
 
