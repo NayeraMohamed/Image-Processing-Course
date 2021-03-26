51750561ec3d06e3bf27e71a6244e53bfd7b54e7
Reem Osama <32741801+ReemOsama99@users.noreply.github.com>
2021-03-26 11:04:41 +0200

    Update RemoveColoredBackground.m
    Code added

--- Project - Statistical Charts Reading and Interpreting/Code & TestCases/RemoveColoredBackground.m
+++ Project - Statistical Charts Reading and Interpreting/Code & TestCases/RemoveColoredBackground.m
@@ -1,1 +1,11 @@
-
+function [R] = removeColoredBackground(R)  
+for i = 1:size(R, 1)
+      for j=1:size(R,2)
+          if ((R(i,j,1)>=150 && R(i,j,1)<=200) && (R(i,j,2)>=200 && R(i,j,2)<=255) &&(R(i,j,3)>=200 && R(i,j,3)<=255))
+               R(i,j,1)=255;
+               R(i,j,2)=255;
+               R(i,j,3)=255;
+          end
+      end
+end
+end
